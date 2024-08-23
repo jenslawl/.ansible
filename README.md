bf82152797193d07a7c841113d8d37f759db1f60
Thomas Rindom <201803775@post.au.dk>
2024-08-23 21:15:59 +0200

    update roles

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # .ansible
+
+Execuate a playbook with the following command:
+```bash
+ansible-playbook -i inventory/hosts.ini playbooks/setup.yml
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-brew update
-brew install ansible
+if [ "$(uname)" == 'Darwin' ]; then
+    echo "Mac OS X"
+    brew update
+    brew install ansible
+
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    echo "Linux"
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+
